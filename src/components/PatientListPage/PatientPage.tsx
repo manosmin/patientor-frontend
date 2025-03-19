04432530cbf36d8b184f421679d2ae3897edfcf6
manosmin <manosmin@hotmail.com>
2025-03-19 20:11:09 +0200

    9.24

--- src/components/PatientListPage/PatientPage.tsx
+++ src/components/PatientListPage/PatientPage.tsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { Patient } from "../../types";
+import { Entry, Patient } from "../../types";
 import { useEffect, useState } from "react";
 import patientService from "../../services/patients";
 
@@ -17,12 +17,59 @@ const PatientPage = () => {
     void fetchPatient();
   }, [id]);
 
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+
+  const diagnosesContent = (e: Entry) => {
+    switch (e.type) {
+      case "HealthCheck":
+        return (
+          <div>
+            <p><strong>Date: </strong>{e.date}</p>
+            <p><strong>Description: </strong>{e.description}</p>
+            <strong>Diagnosis codes: </strong>{e.diagnosisCodes && <ul>
+              {e.diagnosisCodes.map(d => <li key={e.id.concat(d)}>{d}</li>)}
+            </ul>}
+          </div>
+        );
+      case "Hospital":
+        return (
+          <div>
+            <p><strong>Date: </strong>{e.date}</p>
+            <p><strong>Description: </strong>{e.description}</p>
+            <strong>Diagnosis codes: </strong>{e.diagnosisCodes && <ul>
+              {e.diagnosisCodes.map(d => <li key={e.id.concat(d)}>{d}</li>)}
+            </ul>}
+          </div>
+        );
+      case "OccupationalHealthcare":
+        return (
+          <div>
+            <p><strong>Date: </strong>{e.date}</p>
+            <p><strong>Description: </strong>{e.description}</p>
+            <strong>Diagnosis codes: </strong>{e.diagnosisCodes && <ul>
+              {e.diagnosisCodes.map(d => <li key={e.id.concat(d)}>{d}</li>)}
+            </ul>}
+          </div>
+        );
+      default:
+        return assertNever(e);
+    }
+  }
+
   return (
     patient && (
       <div>
         <h2>{patient.name}</h2>
         <p>{patient.ssn}</p>
         <p>{patient.occupation}</p>
+        <h3>Entries</h3>
+        <div>
+          {patient.entries.map((e) => diagnosesContent(e))}
+        </div>
       </div>
     )
   );
--- src/types.ts
+++ src/types.ts
@@ -17,6 +17,49 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
+
+export interface BaseEntry {
+  id: string;
+  date: string;
+  specialist: string;
+  description: string;
+  diagnosisCodes?: Diagnosis["code"][];
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  }
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
